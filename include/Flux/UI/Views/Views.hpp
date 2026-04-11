6e5e9ca721fb62c7de37ce83b7e9508209f3bcdb
Abdurrahman Avcı <abdurrahmanavci@gmail.com>
2026-04-11 22:00:10 +0300

    refactor: remove Picker component and related examples for codebase cleanup

--- include/Flux/UI/Views/Views.hpp
+++ include/Flux/UI/Views/Views.hpp
@@ -4,29 +4,27 @@
 ///
 /// Part of the Flux public API.
 
-
 /// Built-in leaf views (no `Element` dependency).
+#include <Flux/UI/Views/Image.hpp>
+#include <Flux/UI/Views/Line.hpp>
+#include <Flux/UI/Views/PathShape.hpp>
 #include <Flux/UI/Views/Rectangle.hpp>
 #include <Flux/UI/Views/Text.hpp>
-#include <Flux/UI/Views/Image.hpp>
-#include <Flux/UI/Views/PathShape.hpp>
-#include <Flux/UI/Views/Line.hpp>
 
 /// Layout containers (`#include <Flux/UI/Element.hpp>`).
-#include <Flux/UI/Views/VStack.hpp>
-#include <Flux/UI/Views/HStack.hpp>
-#include <Flux/UI/Views/ZStack.hpp>
-#include <Flux/UI/Views/Grid.hpp>
+#include <Flux/UI/Views/Alert.hpp>
+#include <Flux/UI/Views/Button.hpp>
+#include <Flux/UI/Views/Checkbox.hpp>
 #include <Flux/UI/Views/ForEach.hpp>
-#include <Flux/UI/Views/Spacer.hpp>
+#include <Flux/UI/Views/Grid.hpp>
+#include <Flux/UI/Views/HStack.hpp>
+#include <Flux/UI/Views/Icon.hpp>
+#include <Flux/UI/Views/Popover.hpp>
 #include <Flux/UI/Views/ScrollView.hpp>
+#include <Flux/UI/Views/Slider.hpp>
+#include <Flux/UI/Views/Spacer.hpp>
 #include <Flux/UI/Views/TextInput.hpp>
 #include <Flux/UI/Views/Toggle.hpp>
-#include <Flux/UI/Views/Slider.hpp>
-#include <Flux/UI/Views/Checkbox.hpp>
-#include <Flux/UI/Views/Icon.hpp>
-#include <Flux/UI/Views/Picker.hpp>
-#include <Flux/UI/Views/Button.hpp>
-#include <Flux/UI/Views/Alert.hpp>
-#include <Flux/UI/Views/Popover.hpp>
 #include <Flux/UI/Views/Tooltip.hpp>
+#include <Flux/UI/Views/VStack.hpp>
+#include <Flux/UI/Views/ZStack.hpp>
